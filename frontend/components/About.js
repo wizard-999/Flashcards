8e8d8b488d68ba28d7b62855f79ee00eb1430a8d
wizard-999 <152674949+wizard-999@users.noreply.github.com>
2024-08-13 00:09:17 +0530

    Create About.js

---
+++ frontend/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>This app helps you manage and study flashcards effectively.</p>
+    </div>
+  );
+}
+
+export default About;
